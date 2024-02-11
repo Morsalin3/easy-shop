ed8e99968980c7565ed4749badddccc22c6a617d
Morsalin3 <mdmorsalin3@gmail.com>
2024-02-12 01:17:43 +0600

    added banner

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,16 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menus");
+const hamburger = document.querySelector(".nav-burger");
+const remove = document.querySelector(".remove");
+const navMenu = document.querySelector(".nav-container");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+  // hamburger.classList.toggle("active");
+  navMenu.classList.add("active");
+})
+remove.addEventListener("click", () => {
+  navMenu.classList.remove("active");
 })
 
 document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
+  // hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
