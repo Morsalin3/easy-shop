3c74c8eb2c3d0d45cac0c2d8e1794b1776499c40
Morsalin3 <mdmorsalin3@gmail.com>
2024-02-15 23:33:27 +0600

    update category

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,4 @@
+
 const hamburger = document.querySelector(".nav-burger");
 const remove = document.querySelector(".remove");
 const navMenu = document.querySelector(".nav-container");
@@ -5,12 +6,12 @@ const navMenu = document.querySelector(".nav-container");
 hamburger.addEventListener("click", () => {
   // hamburger.classList.toggle("active");
   navMenu.classList.add("active");
-})
+});
 remove.addEventListener("click", () => {
   navMenu.classList.remove("active");
-})
+});
 
 document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click", () => {
   // hamburger.classList.remove("active");
   navMenu.classList.remove("active");
-}))
+}));
